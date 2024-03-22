fa4056c69c2afbcfb5f55ab9f586c41931ded171
Sardi Irfansyah <64241627+Sardiirfan27@users.noreply.github.com>
2024-03-22 17:20:41 +0700

    Update home.py

--- home.py
+++ home.py
@@ -6,6 +6,8 @@ import streamlit.components.v1 as components
 def home_page():
     centered_title = """
     <h1 style="text-align: center;">Deteksi Pneumonia Menggunakan CNN</h1>
+    ![Pneumonia Image](https://www.gavi.org/sites/default/files/vaccineswork/2022/Thumbnail/Pneumonia-picture-1_h2.jpg)
+    **Source:** Global Burden of Disease, 2019. [https://www.healthdata.org/gbd/2019](https://www.healthdata.org/gbd/2019)
     """
     st.markdown(centered_title, unsafe_allow_html=True)
 
@@ -15,7 +17,10 @@ def home_page():
     <p>Pneumonia komuniti atau community-acquired pneumonia (CAP) merupakan pneumonia yang didapat di masyarakat, di mana infeksi terjadi di luar fasilitas pelayanan kesehatan. 
     Pneumonia sendiri didefinisikan sebagai inflamasi parenkim paru yang disebabkan oleh infeksi mikroorganisme patogen, seperti bakteri, virus, jamur, dan parasit; serta pajanan bahan kimia ke parenkim paru.<a id='#ref1' href=#ref style ="color :#003f88;">[1]</a>
     </p>
-
+    <p>
+    Secara global, pneumonia adalah penyebab utama kematian pada anak di bawah usia lima tahun, dan penyebab kematian keempat di segala usia. Satu orang meninggal karena pneumonia setiap 13 detik dan mereka yang berisiko terbesar adalah anak-anak (<5 tahun) dan orang tua (70+ tahun). 
+    Pada tahun 2019, 672.000 dari 2,5 juta kematian pneumonia terjadi di antara anak balita, dengan 70% dari kematian balita (470.400) terjadi di antara anak di bawah usia 12 bulan.<a id='#ref1' href='https://www.gavi.org/vaccineswork/every-death-counts-pneumonia-five-charts' style ="color :#003f88;">[2]</a>
+    </p>
     """
     st.markdown(paragraph, unsafe_allow_html=True)
 
